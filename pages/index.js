4a91c95c61ff04d6e33cd5f33cdc35d5a54d1eaa
David Rupell <drupell7@gmail.com>
2020-05-22 15:44:47 -0400

    Adjusted index to use new InfoCard layout. InfoCard was made reusable, so the content that was originally stored there was moved here, and the InfoCard tag is now used.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import './index.css';
 import React from 'react';
 import data from '../data/data.json';
-import InfoCard from './InfoCard';
 import Layout from "../components/Layout";
+import InfoCard from './InfoCard';
+import Link from "next/link";
 
 
 /**
@@ -19,16 +20,27 @@ export default class Index extends React.Component{
     }
 
     render() {
-        // noinspection JSUnresolvedFunction
         return(
                 <Layout>
-                    <div>
-                        {
-                            this.props.infoCards.map((infoCard)=>( //Creates an InfoCard for each object in data.json
-                                <InfoCard key={infoCard.id}/> //designates the key as the card's id (from data.json)
-                            ))
-                        }
-                    </div>
+                    <InfoCard>
+                        <h1>About Me</h1>
+                        <p>
+                            I graduated from Camden County College (CCC) after Fall of 2019 with a double major in
+                            Computer Science and Mathematics, and a cumulative GPA of 3.959. Currently, I am studying
+                            full time at Rowan University as a Computer Science major with a minor in Mathematics and
+                            a concentration in Cyber Security Defense. I currently have a GPA of 4.000 and continue to remain
+                            involved on campus as a member of Rowan's Association of Computing Machinery and
+                            Cyber Security Club.<br/><br/>
+                            <Link href={"/resume"}>
+                                <button>Resume</button>
+                            </Link>
+                            <br/><br/>
+                            I am currently attending with the New Jersey STARS II scholarship, and I am a member of the Phi
+                            Theta Kappa Honors Society. I have been recognized at CCC through the Miller Mathematics Award
+                            and Program Excellence Awards following my graduation. I have also been recognized on the
+                            President's List at CCC for each term that I have attended.<br/>
+                        </p>
+                    </InfoCard>
                 </Layout>
         )
     }
