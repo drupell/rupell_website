e5ca300e46e56bc4d853def5c609259f2a2f2633
David Rupell <drupell7@gmail.com>
2020-05-24 16:16:10 -0400

    Added external links and buttons to contact page. New links go to my Github profile, and LinkedIn profile. Removed the TODO as well now that it has been completed.

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,6 @@ import InfoCard from "./InfoCard"
  * Contact info page that is accessible via a header link.
  * Currently acts as a placeholder.
  *
- * TODO: Add InfoCards or other styling to the page for cleaner display of information.
  * @author drupell
  */
 
@@ -17,6 +16,13 @@ export default () => (
             <h1>Contact Info</h1>
             <h2>Email:</h2>
             <p>drupell7@gmail.com</p>
+            <h2>Other links:</h2>
+            <a href={"https://github.com/drupell"}>
+                <button>Github</button>
+            </a>
+            <a href={"https://www.linkedin.com/in/david-rupell-40ab5a14b/"}>
+            <button>LinkedIn</button>
+            </a>
         </InfoCard>
     </Layout>
 );
